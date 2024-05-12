a801fbbfd2eed191d9d243f80756e3bdc5221e35
Miguel <migeul.olegna1@gmail.com>
2024-05-13 00:11:38 +0100

    Word

---
+++ script.js
@@ -0,0 +1,28 @@
+//botao word
+// Seleciona a imagem pelo ID
+var imagem = document.getElementById("wordImage");
+
+// Adiciona um evento de clique à imagem
+imagem.addEventListener("click", function() {
+    // Redireciona para a página "Word.html"
+    window.location.href = "Word.html";
+});
+
+//botao powerpoint
+// Seleciona a imagem pelo ID
+var imagem2 = document.getElementById("powerpointImage");
+// Adiciona um evento de clique à segunda imagem
+imagem2.addEventListener("click", function() {
+    // Redireciona para o destino desejado
+    window.location.href = "Powerpoint.html";
+});
+
+//botao exel
+// Seleciona a imagem pelo ID
+var imagem2 = document.getElementById("exelImage");
+// Adiciona um evento de clique à segunda imagem
+imagem2.addEventListener("click", function() {
+    // Redireciona para o destino desejado
+    window.location.href = "Exel.html";
+});
+
