c2b8e7f4420cc89ec66137459606d1e895c34beb
miguelolegna <miguel.olegna1@gmail.com>
2024-05-13 17:11:14 +0100

    merge

---
+++ script.js
@@ -0,0 +1,28 @@
+//botao word
+// Seleciona a imagem pelo ID
+var imagem = document.getElementById("wordImage");
+
+// Adiciona um evento de clique à imagem
+imagem.addEventListener("click", function() {
+    // Redireciona para a página "Word.html"
+    window.location.href = "Word.html";
+});
+
+//botao powerpoint
+// Seleciona a imagem pelo ID
+var imagem2 = document.getElementById("powerpointImage");
+// Adiciona um evento de clique à segunda imagem
+imagem2.addEventListener("click", function() {
+    // Redireciona para o destino desejado
+    window.location.href = "Powerpoint.html";
+});
+
+//botao excel
+// Seleciona a imagem pelo ID
+var imagem2 = document.getElementById("excelImage");
+// Adiciona um evento de clique à segunda imagem
+imagem2.addEventListener("click", function() {
+    // Redireciona para o destino desejado
+    window.location.href = "Excel.html";
+});
+
